e5a5a810cf068e395d056203b8d360593891c0e7
hmtmcse <hmtm.cse@gmail.com>
2023-10-24 19:58:19 +0600

    Working on various things

---
+++ index.tsx
@@ -0,0 +1,13 @@
+import { RapidException } from "./src/artifacts/common/rr-exception";
+import RapidLoadDataPrams from "./src/artifacts/data/rr-load-data-prams";
+import RapidHTTResponse from "./src/artifacts/processor/http/rr-http-response";
+
+// For Interface
+export type {
+    RapidHTTResponse
+}
+
+export {
+    RapidException,
+    RapidLoadDataPrams
+}
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
-        main: './src/example/index.tsx'
+        main: './src/index.tsx'
     },
     mode: 'development',
     module: {
