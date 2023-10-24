19cd4defbd5cb24d76aaa713902fbc308cb86b5c
hmtmcse <hmtm.cse@gmail.com>
2023-10-24 23:15:47 +0600

    Fixing UI Error

--- index.tsx
+++ index.tsx
@@ -7,6 +7,14 @@ import RapidLayoutInfoData from "./src/artifacts/data/rr-layout-info-data";
 import {RapidProps} from "./src/artifacts/interface/rr-mixed-interface";
 import RapidComponentState from "./src/artifacts/component/rr-component-state";
 import RapidComponent from "./src/artifacts/component/rr-component";
+import {FieldSpecification} from "./src/artifacts/data/rr-input-definition";
+import RapidSysComponent from "./src/artifacts/component/rr-sys-component";
+import RapidReactComponent from "./src/artifacts/component/rr-react-component";
+import RapidLayoutRenderer from "./src/artifacts/component/rr-layout-rander";
+import {_t, i18n, _tNumber} from "react-rapid-i18n/app/react-rapid-i18n";
+import {RapidUtil} from "./src/artifacts/utils/rr-util";
+import RapidBrowserStorageManager from "./src/artifacts/manager/rr-browser-storage-manager";
+import {RapidAppContext} from "./src/artifacts/config/rr-app-context";
 
 // For Interface
 export type {
@@ -15,6 +23,9 @@ export type {
 }
 
 export {
+    _tNumber,
+    _t,
+    i18n,
     RapidException,
     RapidLoadDataPrams,
     RapidAppConfig,
@@ -22,13 +33,24 @@ export {
     RapidLayoutInfoData,
     RapidComponentState,
     RapidComponent,
+    FieldSpecification,
+    RapidSysComponent,
+    RapidReactComponent,
+    RapidLayoutRenderer,
+    RapidBrowserStorageManager,
+    RapidAppContext,
+    RapidUtil,
 }
 
 // External Library Export
 import React from "react";
 import {Component} from 'react';
+import {Link} from "react-router-dom";
+import {Redirect} from "react-router";
 
 export {
     Component,
-    React
+    React,
+        Link,
+    Redirect
 }
