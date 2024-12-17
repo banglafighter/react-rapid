84153bb653d88cafe04bd9ce7e3e9b76475ddd49
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2024-12-17 21:30:36 +0600

    allow zero to input value, if null then ignore

--- src/artifacts/component/helper/rr-component-helper.tsx
+++ src/artifacts/component/helper/rr-component-helper.tsx
@@ -87,7 +87,7 @@ export class RapidComponentHelper {
     }
 
     public getValueFromFormData(name: string, defaultValue: any = "") {
-        if (this.state.formData && this.state.formData[name]) {
+        if (this.state.formData && this.state.formData[name] != null) {
             return this.state.formData[name];
         }
         return defaultValue;
